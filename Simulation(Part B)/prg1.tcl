23971b1240d30e8ce7a0c5cab40573778dc07b9d
Vinodraj VK <tecraj19@gmail.com>
2023-12-25 20:08:51 +0530

    Edited prg1.tcl

--- Simulation(Part B)/prg1.tcl
+++ Simulation(Part B)/prg1.tcl
@@ -1,7 +1,7 @@
 set ns [new Simulator]
-set ntrace[open prg1.tr w]
+set ntrace [open prg1.tr w]
 $ns trace-all $ntrace
-set namfile[open prg1.nam w]
+set namfile [open prg1.nam w]
 $ns namtrace-all $namfile 
 
 proc Finish{}{
@@ -10,23 +10,26 @@ proc Finish{}{
     close $ntrace
     close $namfile
 
-    exec prg1.nam &
+    exec nam prg1.nam &
     exec echo "The number of Packets dropped is" &
     exec grep -c "^d" prg1.tr &
     exit 0
 }
 
-set n0[$ns node]
-set n1[$ns node]
-set n2[$ns node]
+set n0 [$ns node]
+set n1 [$ns node]
+set n2 [$ns node]
 
 $n0 label "TCP Source"
 $n2 label "Sink"
 
-$ns color blue
+$ns color 1 blue
 
 $ns duplex-link $n0 $n1 0.5Mb 10ms DropTail
 $ns duplex-link $n1 $n2 0.5Mb 10ms DropTail
+
+$ns queue-limit $n0 $n1 10
+$ns queue-limit $n1 $n2 10
 
 set tcp0 [new Agent/TCP]
 $ns attach-agent $n0 $tcp0
@@ -41,7 +44,7 @@ $cbr0 set rate_ 0.5Mb
 $cbr0 attach-agent $tcp0
 $tcp0 set class_ 1
 
-$ns at 0.0 "$cbr0 Start"
+$ns at 0.0 "$cbr0 start"
 $ns at 5.0 "Finish"
 
 $ns run
